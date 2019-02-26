b44416791ad6f0277472eec4b12713c484697b32
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2019-02-26 20:13:18 +0200

    views content and helper for it

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,13 @@
 module CommentsHelper
+  def author_user?(comm)
+    unless comm.try(:commentator)
+      return false
+    else
+      if comm.commentator.eql?('User')
+        @usr = User.find(id: comm.commentator_id)
+      elsif comm.commentator.eql?('AdminUser')
+        @usr = AdminUser.find(id: comm.commentator_id)
+      end
+    end
+  end
 end
